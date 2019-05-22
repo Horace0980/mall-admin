d42f1b2f4338d3138b99e1a22f136e95d7643ae3
TcomcXF <aert1094@qq.com>
2019-05-22 21:36:09 +0800

    xf V0.0.2

---
+++ src/main/java/com/cskaoyan/malladmin/bean/user/PageHandler.java
@@ -0,0 +1,80 @@
+package com.cskaoyan.malladmin.bean.user;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @Author: xf
+ * @Date: 2019/5/22 21:04
+ */
+@Component
+public class PageHandler<T> {
+    private int total;
+    private int pages;
+    private int limit;
+    private int oage;
+    private List<T> list;
+
+    public PageHandler() {
+    }
+
+    public PageHandler(int total, int pages, int limit, int oage, List<T> list) {
+        this.total = total;
+        this.pages = pages;
+        this.limit = limit;
+        this.oage = oage;
+        this.list = list;
+    }
+
+    @Override
+    public String toString() {
+        return "PageHandler{" +
+                "total=" + total +
+                ", pages=" + pages +
+                ", limit=" + limit +
+                ", oage=" + oage +
+                ", list=" + list +
+                '}';
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+
+    public int getOage() {
+        return oage;
+    }
+
+    public void setOage(int oage) {
+        this.oage = oage;
+    }
+
+    public List<T> getList() {
+        return list;
+    }
+
+    public void setList(List<T> list) {
+        this.list = list;
+    }
+}
---
+++ src/main/java/com/cskaoyan/malladmin/mapper/user/UserMapper.java
@@ -0,0 +1,8 @@
+package com.cskaoyan.malladmin.mapper.user;
+
+/**
+ * @Author: xf
+ * @Date: 2019/5/22 21:13
+ */
+public class UserMapper {
+}
