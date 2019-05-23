8fe7b9eb9e08736eee01ac5362622d0193b3ee7d
alan-ZCH <1136559229@qq.com>
2019-05-23 21:27:28 +0800

    修改一下

--- src/main/java/com/cskaoyan/malladmin/controller/stat/StatController.java
+++ src/main/java/com/cskaoyan/malladmin/controller/stat/StatController.java
@@ -6,8 +6,6 @@ import com.cskaoyan.malladmin.vo.QueryVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
