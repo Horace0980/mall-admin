1692cbda3985fb9fb2cd6a2be5e43417418a0397
pikachu <13298328928@163.com>
2019-05-24 16:32:29 +0800

    完善首页

---
+++ src/main/java/com/cskaoyan/malladmin/service/log/DashboardService.java
@@ -0,0 +1,11 @@
+package com.cskaoyan.malladmin.service.log;
+
+import com.cskaoyan.malladmin.vo.QueryVo;
+
+/**
+ * @Author: Qiu
+ * @Date: 2019/5/24 16:16
+ */
+public interface DashboardService {
+  QueryVo dashboard();
+}
