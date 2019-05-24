afbca381e51cefae048b50ff4dcc46d0277b671f
Dankin <38831429+Dankin@users.noreply.github.com>
2019-05-24 16:56:30 +0800

    Merge branch 'master' of https://github.com/2968yyc/mall-admin into me
    # Conflicts:
    #	pom.xml
    #	src/main/java/com/cskaoyan/malladmin/controller/storage/StorageControler.java
    #	src/main/java/com/cskaoyan/malladmin/service/admin/impl/AdminServiceImpl.java

---
+++ src/main/java/com/cskaoyan/malladmin/bean/permission/PermissionData.java
@@ -0,0 +1,14 @@
+package com.cskaoyan.malladmin.bean.permission;
+
+import java.util.List;
+
+/**
+ * @Author: Dankin
+ * @Date: 2019/5/24 16:36
+ */
+public class PermissionData {
+    private List assignedPermissions;
+
+
+
+}
